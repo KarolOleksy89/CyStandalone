997132c963ff6d82cddae537456f7368cd8158fe
Mateusz Drozdzik <mateusz.drozdzik@playbookengineering.com>
2022-09-02 13:19:39 +0200

    second commit

---
+++ cypress/e2e/google.spec.ts
@@ -0,0 +1,10 @@
+describe('Test suite name', () => {
+
+    beforeEach(() => {
+        // write methods to run before each test
+    });
+
+    it('Test title', () => {
+        // write the test
+    });
+});
--- cypress/e2e/login.spec.cy.js
+++
@@ -1,5 +0,0 @@
-describe('Login/Logout', () => {
-    it('C2184 - should login with active account', () => {
-        cy.visit('https://google.com');
-    });
-});
